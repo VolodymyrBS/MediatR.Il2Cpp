d94367436399d8a83baddd33d7efcac32f411ace
Volodymyr Stefanyshyn <v.b.stefanyshyn@gmail.com>
2020-10-31 02:51:50 +0200

    Access to RequestHanlerWrapperImpl

---
+++ HandlerStub.cs
@@ -0,0 +1,11 @@
+﻿namespace MediatR.Il2Cpp
+{
+    public class HandlerStub
+    {
+        public static object MakeStub<TRequest, TResponse>()
+            where TRequest : IRequest<TResponse>
+        {
+            return new MediatR.Internal.RequestHandlerWrapperImpl<TRequest,TResponse>();
+        }
+    }
+}
